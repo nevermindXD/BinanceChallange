238c13f95f14c597671f86db92bb397fb2d1bda2
Juan Carlos Olivier <juanc.olivierj@gmail.com>
2021-10-14 00:23:22 -0500

    id presave

--- src/models/currencies.model.ts
+++ src/models/currencies.model.ts
@@ -4,9 +4,7 @@ import { Currency } from '../interfaces/currencies.interface';
 const currencySchema: Schema = new Schema({
   _id: {
     type:Number,
-    default: 1,
-    unique: true,
-    index: true
+    default: 0
   },
   symbol: {
     type: String,
@@ -25,9 +23,12 @@ currencySchema.set('toJSON', {
 
 
 currencySchema.pre('save', function(next) {
-  var doc = this;
-
-  next();
+  this.constructor.find().then((result) => {
+    console.log('find',result)
+    this._id = result.length + 1;
+    console.log('this',this)
+    next();
+  });
 });
 
 const currencyModel = model<Currency & Document>('Currency', currencySchema);
